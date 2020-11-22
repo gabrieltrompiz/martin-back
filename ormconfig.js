f1989040aab53f16e34c9934cd47e3690d607ef5
Gabriel Trompiz <gab.tc@icloud.com>
2020-11-22 18:58:00 -0400

    cleaned up package.json scripts and added migrations folder to ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -1,3 +1,7 @@
+const isLocal = !['test', 'production'].includes(process.env.NODE_ENV)
+const folder =  !isLocal ? 'build' : 'src';
+const format = !isLocal ? 'js' : 'ts';
+
 module.exports = {
   "type": 'postgres',
   "host": process.env.DB_HOSTNAME,
@@ -6,5 +10,13 @@ module.exports = {
   "username": process.env.DB_USERNAME,
   "password": process.env.DB_PASSWORD,
   "database": process.env.DB_NAME,
-  "synchronize": true
+  "synchronize": process.env.NODE_ENV !== 'production',
+  "logging": true,
+  "entities":  [`${folder}/entities/**/*.${format}`],
+  "factories": [`${folder}/factories/**/*.${format}`],
+  "migrations": [`${folder}/migrations/**/*.${format}`],
+  "cli": {
+    "entitiesDir": "src/entities",
+    "migrationsDir": "src/migrations",
+  }
 }
