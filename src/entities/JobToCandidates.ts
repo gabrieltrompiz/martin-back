6e24b8b5548b5204ae1a7204151a7e6a64b7b9c7
Gabriel Trompiz <gab.tc@icloud.com>
2020-12-24 10:27:07 -0400

    fixed entity that shouldn't be in graphQL schema

--- src/entities/JobToCandidates.ts
+++ src/entities/JobToCandidates.ts
@@ -1,4 +1,4 @@
-import { Field, ID, Int, ObjectType, registerEnumType } from '@nestjs/graphql';
+import { registerEnumType } from '@nestjs/graphql';
 import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { JobPosting } from './JobPosting';
 import { User } from './User';
@@ -12,38 +12,30 @@ export enum Stage {
 };
 registerEnumType(Stage, { name: 'Stage' });
 
-@ObjectType()
 @Entity({ name: 'job_candidate' })
 export class JobToCandidate {
 
-  @Field(type => ID)
   @PrimaryGeneratedColumn()
   id: number
 
-  @Field(type => Int)
   @Column()
   jobId: number;
 
-  @Field(type => Int)
   @Column()
   userId: number;
 
-  @Field(type => String)
   @CreateDateColumn()
   appliedIn: Date
 
-  @Field(type => Stage)
   @Column({
     type: 'enum',
     enum: Stage
   })
   stage: Stage
 
-  @Field(type => JobPosting)
   @ManyToOne(() => JobPosting, jobPosting => jobPosting.candidates)
   job: JobPosting;
 
-  @Field(type => User)
   @ManyToOne(() => User, user => user.appliedTo)
   user: User;
 
